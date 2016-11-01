cda4a590c563b75c2703611d96fd9b048840118d
beaulirl <beaulirl@gmail.com>
2016-11-01 17:04:29 +0300

    array add

--- exercise3/EnumTry.java
+++ exercise3/EnumTry.java
@@ -1,7 +1,6 @@
 package ThirdExercise;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -51,123 +50,82 @@ public class EnumTry {
 
         long startTime3 = System.nanoTime();
         ArrayList<Integer> array = new ArrayList<>();
+        int[] array_amount = new int[10];
 
-        Map<Integer, Integer> hashmap = new HashMap<>();
-        int f1 = 1;
-        int f2 = 1;
-        int f3 = 1;
-        int f4 = 1;
-        int f5 = 1;
-        int f6 = 1;
-        int f7 = 1;
-        int f8 = 1;
-        int f9 = 1;
-        int f10 = 1;
-        int f11 = 1;
         for (int i = 0; i < 50; i++) {
             int s = (int) (Math.random() * 10);
             array.add(s);
             if (s == 0) {
-                hashmap.put(0, f1);
-                f1++;
+                array_amount[0] += 1;
             } else if (s == 1) {
-                hashmap.put(1, f2);
-                f2++;
+                array_amount[1] += 1;
             } else if (s == 2) {
-                hashmap.put(2, f3);
-                f3++;
+                array_amount[2] += 1;
             } else if (s == 3) {
-                hashmap.put(3, f4);
-                f4++;
+                array_amount[3] += 1;
             } else if (s == 4) {
-                hashmap.put(4, f5);
-                f5++;
+                array_amount[4] += 1;
             } else if (s == 5) {
-                hashmap.put(5, f6);
-                f6++;
+                array_amount[5] += 1;
             } else if (s == 6) {
-                hashmap.put(6, f7);
-                f7++;
+                array_amount[6] += 1;
             } else if (s == 7) {
-                hashmap.put(7, f8);
-                f8++;
+                array_amount[7] += 1;
             } else if (s == 8) {
-                hashmap.put(8, f9);
-                f9++;
+                array_amount[8] += 1;
             } else if (s == 9) {
-                hashmap.put(9, f10);
-                f10++;
+                array_amount[9] += 1;
             } else if (s == 10) {
-                hashmap.put(10, f11);
-                f11++;
+                array_amount[10] += 1;
             }
         }
         long estimatedTime3 = System.nanoTime() - startTime3;
         System.out.println(array);
-        System.out.println(hashmap);
+        for(int s = 0; s < array_amount.length; s++){
+            System.out.print(s + " - " + array_amount[s] + "; ");
+        }
+        System.out.println();
         System.out.println(estimatedTime3);
 
         long startTime4 = System.nanoTime();
         ArrayList<Integer> array1 = new ArrayList<>();
-        Map<Integer, Integer> hashmap1 = new HashMap<>();
-        int s1 = 1;
-        int s2 = 1;
-        int s3 = 1;
-        int s4 = 1;
-        int s5 = 1;
-        int s6 = 1;
-        int s7 = 1;
-        int s8 = 1;
-        int s9 = 1;
-        int s10 = 1;
-        int s11 = 1;
+        int[] array_amount1 = new int[10];
         for (int i = 0; i < 50; i++) {
             int s = (int) (Math.random() * 10);
             array1.add(s);
             switch(s){
                 case 0:
-                    hashmap1.put(0, s1);
-                    s1++;
+                    array_amount1[0] += 1;
                     break;
                 case 1:
-                    hashmap1.put(1, s2);
-                    s2++;
+                    array_amount1[1] += 1;
                     break;
                 case 2:
-                    hashmap1.put(2, s3);
-                    s3++;
+                    array_amount1[2] += 1;
                     break;
                 case 3:
-                    hashmap1.put(3, s4);
-                    s4++;
+                    array_amount1[3] += 1;
                     break;
                 case 4:
-                    hashmap1.put(4, s5);
-                    s5++;
+                    array_amount1[4] += 1;
                     break;
                 case 5:
-                    hashmap1.put(5, s6);
-                    s6++;
+                    array_amount1[5] += 1;
                     break;
                 case 6:
-                    hashmap1.put(6, s7);
-                    s7++;
+                    array_amount1[6] += 1;
                     break;
                 case 7:
-                    hashmap1.put(7, s8);
-                    s8++;
+                    array_amount1[7] += 1;
                     break;
                 case 8:
-                    hashmap1.put(8, s9);
-                    s9++;
+                    array_amount1[8] += 1;
                     break;
                 case 9:
-                    hashmap1.put(9, s10);
-                    s10++;
+                    array_amount1[9] += 1;
                     break;
                 case 10:
-                    hashmap1.put(10, s11);
-                    s11++;
+                    array_amount1[10] += 1;
                     break;
                 default:
                     System.out.println();
@@ -176,7 +134,10 @@ public class EnumTry {
         }
         long estimatedTime4 = System.nanoTime() - startTime4;
         System.out.println(array1);
-        System.out.println(hashmap1);
+        for(int s = 0; s < array_amount1.length; s++){
+            System.out.print(s + " - " + array_amount1[s] + "; ");
+        }
+        System.out.println();
         System.out.println(estimatedTime4);
 
 
